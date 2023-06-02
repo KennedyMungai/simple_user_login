15736cf6e6d38537048b7b71a64306348055e1f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 12:22:13 +0300

    Reorganized the imports

--- auth/authentication.py
+++ auth/authentication.py
@@ -1,6 +1,7 @@
 """The app that holds the authentication code"""
+from datetime import datetime, timedelta
+
 import jwt
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
