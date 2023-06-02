e45a5eb20396e2d32a9e34e7bce20620366859d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 12:04:40 +0300

    Created the schemas to be used by the user

--- schemas/users_schema.py
+++ schemas/users_schema.py
@@ -1,1 +1,32 @@
 """The users schema file"""
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    """The base for the User schemas
+
+    Args:
+        BaseModel (Pydantic): _description_
+    """
+    email: EmailStr
+    
+class UserCreate(UserBase):
+    """The schema for creating a user
+
+    Args:
+        UserBase (Pydantic): The base for creating schemas
+    """
+    password: str
+    
+    
+class User(UserBase):
+    """The schema for the user data
+
+    Args:
+        UserBase (Pydantic): The base schema for the user data
+    """
+    id: int
+    
+    class Config:
+        """The config for the user schema"""
+        orm_mode = True
