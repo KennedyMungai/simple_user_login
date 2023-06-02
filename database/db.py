552ed2695259d9a6948680bc13c33e0447102fbc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 11:30:55 +0300

    Added teh statement that creates the engine

--- database/db.py
+++ database/db.py
@@ -4,6 +4,15 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
 load_dotenv(find_dotenv())
+
+MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_PORT = os.environ.get("MYSQL_PORT")
+MYSQL_DB = os.environ.get("MYSQL_DB")
+
+engine = create_engine(f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}')
