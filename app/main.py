06429a3eaf874f6de1605f423a9475a7fefa470a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-02 11:28:28 +0300

    Made some lines shorter

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@
 from fastapi import FastAPI
 
 
-app = FastAPI(title='Simple User Authentication', description="A simple user authentication backend")
+app = FastAPI(
+    title='Simple User Authentication', 
+    description="A simple user authentication backend"
+    )
 
 
-@app.get("/", name="Root", description="The root endpoint for the app", response_model=dict[str, str])
+@app.get(
+    "/", 
+    name="Root", 
+    description="The root endpoint for the app",
+    response_model=dict[str, str]
+    )
 async def root_endpoint() -> dict[str, str]:
     """The root endpoint for the app
 
